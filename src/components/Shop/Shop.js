ebeb942c00b9d3c03ff7dd01f38e691fce7d480d
unknown <abdullahalomar048@gmail.com>
2022-03-23 17:46:22 +0600

    first commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setproducts] = useState([]);
+    const [cart, setcart] =useState([])
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(response => response.json())
+        .then(json => setproducts(json))
+    },[])
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+        //do not do this cart.push(product);
+        const newCart = [...cart, product];
+        setcart(newCart);
+    } 
+
+    return (
+        <div className='shop-conatiner'>
+            <div className="produtcs-container">
+               {
+                   products.map(product=> <Product key={product.id}
+                    product = {product}
+                    handleAddToCart = {handleAddToCart}
+                   ></Product>)
+               }
+            </div>
+            <div className="cart-container">
+            <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
