58c601022ab0ac87c791e01481bdc675d552cd2a
unknown <abdullahalomar048@gmail.com>
2022-03-24 02:43:05 +0600

    quantity add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,19 +3,21 @@ import './Cart.css'
 
 const Cart = (props) => {
     const {cart} = props;
-    console.log(cart);
+    // console.log(cart);
     let shipping = 0;
     let total = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
-        shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping ;
     }
     const tax = (total * 0.1).toFixed(2);
     const grandtotal = total + shipping + parseFloat(tax);
     return (
         <div className='cart'>
                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping: ${shipping}</p>
                 <p>Tax: ${tax}</p>
